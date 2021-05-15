ecb71766b1e169e6a234cb4817a2b505105e282d
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-05-15 16:40:44 +0300

    header70%

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 // TODO: add js;
+function burgerMenu(selector) {
+  const menu = $(selector);
+  const button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  const links = menu.find('.burger-menu_link');
+  const overlay = menu.find('.burger-menu_overlay');
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu_active');
+
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+}
+
+burgerMenu('.burger-menu');
